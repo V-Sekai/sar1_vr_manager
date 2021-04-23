b640221c51154c9d949a351ac60afe93206ee780
Saracen <SaracenOne@gmail.com>
2021-04-23 01:52:19 +0100

    WIP hand pose support

--- vr_origin.gd
+++ vr_origin.gd
@@ -20,6 +20,7 @@ const vr_component_teleport_const = preload("components/vr_component_teleport.gd
 const vr_component_render_tree_const = preload("components/vr_component_render_tree.gd")
 const vr_component_advanced_movement_const = preload("components/vr_component_advanced_movement.gd")
 const vr_component_lasso_const = preload("components/vr_component_lasso.gd")
+const vr_component_hand_pose_const = preload("components/vr_component_hand_pose.gd")
 
 var components: Array = []
 
@@ -152,6 +153,7 @@ func create_components() -> void:
 	create_and_add_component(vr_component_render_tree_const)
 	create_and_add_component(vr_component_advanced_movement_const)
 	create_and_add_component(vr_component_lasso_const)
+	create_and_add_component(vr_component_hand_pose_const)
 
 func destroy_components() -> void:
 	for component in components:
